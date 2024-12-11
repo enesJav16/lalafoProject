f4003e6404ee0c7c0aea96656480fc262916fa17
nursultan <alymbekovnursultan232@gmail.com>
2024-12-11 23:17:14 +0600

    Merge pull request #5 from enesJav16/first
     removed names

--- src/dao/daoImpl/AnnouncementDaoImpl.java
+++ src/dao/daoImpl/AnnouncementDaoImpl.java
@@ -10,41 +10,41 @@ import java.util.Scanner;
 
 public class AnnouncementDaoImpl implements AnnouncementDao {
 
-    /// ADD ANNOUNCEMENT
+
     @Override
     public String addAnnocement(Annoucement announcement) {
         Database.annoucements.add(announcement);
-        return "success add announcement";
+        return "success add announcement:";
     }
 
-    ///  GET ALL ANNOUNCEMENT
+
     @Override
     public List<Annoucement> getAllAnnouncement() {
         if (Database.annoucements.isEmpty()) {
-            return null;
+             return null;
         }
-        return Database.annoucements;
+         return Database.annoucements;
     }
 
-    /// GET ANNOUNCEMENT BY ID
+
     @Override
     public Annoucement getAnnouncementById(Long id) {
-        if (Database.annoucements == null || id == null) {
+        if (Database.annoucements  ==  null || id == null) {
             return null;
         }
         for (Annoucement announcement : Database.annoucements) {
             if (announcement.getId().equals(id)) {
-                return announcement;
+                 return announcement;
 
             }
         }
-        return null;
+         return null;
     }
 
-    /// UPDATE ANNOUNCEMENT
+
     @Override
     public void updateAnnouncement(Long id, Annoucement announcement) {
-        for (Annoucement announcement2 : Database.annoucements) {
+        for (Annoucement announcement2  :  Database.annoucements) {
             if (id.equals(announcement2.getId())) {
                 announcement2.setName(announcement.getName());
                 announcement2.setDescription(announcement.getDescription());
@@ -52,15 +52,15 @@ public class AnnouncementDaoImpl implements AnnouncementDao {
         }
     }
 
-    /// DELETE ANNOUNCEMENT
+
     @Override
     public void deleteAnnouncement(Long id) {
         Annoucement announcement = getAnnouncementById(id);
         if (announcement != null) {
             Database.annoucements.remove(getAnnouncementById(id));
-            System.out.println("announcement with id " + id + " deleted successfully");
+            System.out.println(" announcement with id " + id + " deleted successfully: ");
         } else {
-            System.out.println("announcement with id " + id + " not found");
+            System.out.println(" announcement with id " + id + " not found: ");
         }
     }
 }
